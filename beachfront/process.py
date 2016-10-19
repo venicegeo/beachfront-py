3d5219c2e69b5639d2d8e856de013fe935a6e6cb
Matthew Hanson <matt.a.hanson@gmail.com>
2016-10-19 01:33:39 -0400

    global otsu and tests

--- beachfront/process.py
+++ beachfront/process.py
@@ -1,25 +1,13 @@
-from osgeo import ogr, osr
-from gippy import GeoImage, GeoVector
-import gippy.algorithms as alg
-import json
 from skimage.filters import threshold_otsu
+from nose.tools import set_trace
 
 
-def threshold(fname, vector):
-    """ Standard thresholding algorithm """
-    # DRAFT
-    geoimg = GeoImage(fname)
-    nodata = geoimg[0].NoDataValue()
+def otsu_threshold(georaster):
+    """ Use Otsu's method to caluclate optimal threshold ignoring nodata values """
+    img = georaster.read()
+    nodata = georaster.nodata()
+    ndinds = img != nodata
+    # img[ndinds] = 0
+    threshold = threshold_otsu(img)
 
-    img = geoimg[0].Read()
-    ndinds = img == nodata
-    img[ndinds] = 0
-    th = threshold_otsu(img)
-
-    inds = img > th
-    img[inds] = 1
-    img[~inds] = 0
-    img[ndinds] = 1
-
-    return img
-
+    return threshold
--- tests/test_process.py
+++ tests/test_process.py
@@ -1,21 +1,37 @@
+from __future__ import print_function
 import unittest
 import os
 from gippy import Options
 from gippy import GeoImage
-import beachfront
+from beachfront import process
+from .utils import create_empty_image
 from nose.tools import set_trace
 
 
 class TestProcess(unittest.TestCase):
     """ Test masking functions """
 
+    testname = os.path.join(os.path.dirname(__file__), 'test.tif')
+
     def _setUp(self):
         Options.set_verbose(4)
         self.test_image = GeoImage('test.tif', 100, 100)
         # TODO - create step function or other pattern in binary image
 
-    def _test_threshold(self):
+    def create_bimodal_image(self):
+        """ Create image with a bimodal distribution """
+        geoimg = create_empty_image()
+        arr = geoimg.read()
+        arr[0:3, 0:10] = 1
+        arr[3:5, 0:10] = 2
+        arr[5:7, 0:10] = 9
+        arr[7:10, 0:10] = 10
+        geoimg[0].write(arr)
+        return geoimg
+
+    def test_threshold(self):
         """ Threshod binary image """
-        geoimg = self.test_image()
-        imgout = beachfront.process.threshold(geoimg)
-        self.assertEqual(imgout.nbands(), 1)
+        geoimg = self.create_bimodal_image()
+        threshold = process.otsu_threshold(geoimg[0])
+        bimg = geoimg[0].read() > threshold
+        self.assertEqual(bimg.mean(), 0.5)
