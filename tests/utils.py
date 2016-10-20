420b76490a502cd93527b78f614b2a2e54a47768
Matthew Hanson <matt.a.hanson@gmail.com>
2016-10-20 00:20:39 -0400

    more test utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,21 +1,32 @@
 import os
 import numpy
+import requests
 from gippy import GeoImage
 
 default_fout = os.path.join(os.path.dirname(__file__), 'test.tif')
 
 
-def create_image(fout=None, xsz=10, ysz=10, nodata=99):
-    """ Create test image """
+def create_image(fout=None, xsz=10, ysz=10, nodata=99, empty=False):
+    """ Create test image, empty means all nodata, otherwise 0  """
     fout = default_fout if fout is None else fout
     geoimg = GeoImage.create(fout, xsz=xsz, ysz=ysz, dtype='byte')
     geoimg.set_nodata(nodata)
+    if not empty:
+        arr = numpy.zeros((geoimg.xsize(), geoimg.ysize()), dtype='uint8')
+        geoimg[0].write(arr)
     return geoimg
 
 
-def create_empty_image():
-    """ Create emptry image """
-    geoimg = create_image()
-    arr = numpy.zeros((geoimg.xsize(), geoimg.ysize()), dtype='uint8')
-    geoimg[0].write(arr)
-    return geoimg
+def download_image(url):
+    """ Download a test image """
+    fout = os.path.join(os.path.dirname(__file__), os.path.basename(url))
+    if not os.path.exists(fout):
+        print('Downloading image %s' % fout)
+        stream = requests.get(url, stream=True)
+        try:
+            with open(fout, 'wb') as f:
+                for chunk in stream.iter_content(1024):
+                    f.write(chunk)
+        except:
+            raise Exception("Problem downloading %s" % url)
+    return GeoImage(fout)
