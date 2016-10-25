ab8e071d3c5d83a4209f82e67d9565e0e9458e20
Matthew Hanson <matt.a.hanson@gmail.com>
2016-10-25 00:20:42 -0400

    masking module + tests

--- beachfront/mask.py
+++ beachfront/mask.py
@@ -1,67 +1,50 @@
-from osgeo import ogr, osr
+from osgeo import gdal, ogr, osr
 from gippy import GeoImage, GeoVector
 import gippy.algorithms as alg
 import json
+from pyproj import Proj, transform
 from nose.tools import set_trace
 
 
-def open_wfs(wfsurl, layername):
-    """ Open a WFS 2.0 endpoint (EPSG 4326) """
-    vec = ogr.Open(wfsurl)
-    layer = vec.GetLayer(layername)
-    return vec, layer
+def fetch_wfs(wfsurl, layer):
+    """ Fetch wfs features within bounding box """
+    wfs = ogr.Open(wfsurl)
+    layer = wfs.GetLayer(layer)
+    return (wfs, layer)
 
 
-def get_features(layer, bbox):
-    """ Get features in bounding box """
-    # assumes bounding box in same projection as layer
-    #filter = 'ST_Intersects(the_geoms, ST_MakeEnvelope([%s], 3857))' % ','.join(str(p) for p in bbox)
-    # for now, assume bounding box in 3857
-    layer.SetSpatialFilterRect(bbox[0], bbox[1], bbox[2], bbox[3])
-    # convert to geojson ?
-    return layer
-
-
-def get_unioned_features(layer, bbox):
-    """ Get union of all features in bounding box """
-    layer = get_features(layer, bbox)
+def get_features(layer, bbox=None, union=False):
+    """ Union together all features in this layer and reutrn as GeoJSON """
+    if bbox is not None:
+        layer.SetSpatialFilterRect(bbox[0], bbox[3], bbox[2], bbox[1])
     poly = ogr.Geometry(ogr.wkbPolygon)
-    for feature in layer:
-        geom = feature.GetGeometryRef().GetLinearGeometry()
-        poly = poly.Union(geom)
+    if union:
+        for feature in layer:
+            geom = feature.GetGeometryRef()
+            # required for ogr2
+            #geom = geom.GetLinearGeometry()
+            poly = poly.Union(geom)
+    if bbox is not None:
+        wkt = "POLYGON ((%s %s, %s %s, %s %s, %s %s, %s %s))" % \
+                (bbox[0], bbox[1], bbox[2], bbox[1], bbox[2], bbox[3], bbox[3], bbox[0], bbox[0], bbox[1])
+        bbox_wkt = ogr.CreateGeometryFromWkt(wkt)
+        poly = poly.Intersection(bbox_wkt)
     return json.loads(poly.ExportToJson())
-
-
-def transform_coords(bbox):
-    """ Convert coordinates in arbitrary projection to EPSG:4326 """
-    srs1 = osr.SpatialReference()
-    srs1.ImportFromEPSG(3857)
-    #srs2 = layer.GetSpatialRef()
-    srs2 = osr.SpatialReference()
-    srs2.ImportFromEPSG(4326)
-    transform = osr.CoordinateTransformation(srs1, srs2)
-    x0, y0, z0 = transform.TransformPoint(bbox[0], bbox[1])
-    x1, y1, z1 = transform.TransformPoint(bbox[2], bbox[3])
-
-    #inproj = Proj(init='EPSG:3857')
-    #outproj = Proj(init=layer.GetSpatialRef())
-    #x0, y0 = transform(inproj, outproj, bbox[0], bbox[1])
-    #x1, y1 = transform(inproj, outproj, bbox[2], bbox[3])
-    #features = geovec.where(filter)
-    return [x0, y0, x1, y1]
     
 
-#def fetch_vector(wfsurl, bbox=None):
-#    """ Get polygons of WFS url intersecting bounding box """
-    
+def mask_with_wfs(geoimg, wfsurl, layer, fout=''):
+    """ Mask geoimage with a WFS """
+    ext = geoimg.geo_extent()
+    wfs, layer = fetch_wfs(wfsurl, layer)
+    feature = get_features(layer, bbox=[ext.x0(), ext.y0(), ext.x1(), ext.y1()], union=True)
+    with open('feature.geojson', 'w') as f:
+        f.write(json.dumps(feature))
 
+    # create GeoVector
+    geovec = GeoVector('feature.geojson')
 
-def mask(fname, vector):
-    geoimg = GeoImage(fname)
-    geovec = GeoVector(vector)
-    imgout = alg.cookie_cutter([geoimg], geovec)
-
-
-def mask_bitmask(geoimg, bitmask):
-    """ Create mask from a bitmask """
-    return
+    res = geoimg.resolution()
+    fout = 'masktest.tif'
+    imgout = alg.cookie_cutter([geoimg], filename=fout,
+            feature=geovec[0], xres=0.003, yres=0.003) #xres=res.x(), yres=res.y())
+    return imgout
--- test/test_mask.py
+++ test/test_mask.py
@@ -1,49 +1,55 @@
 import unittest
 import os
 from gippy import Options
-from gippy import GeoImage
-import beachfront 
+from beachfront import mask
+from .utils import download_image
 from nose.tools import set_trace
+
+# load envvars
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 
 class TestMask(unittest.TestCase):
     """ Test masking functions """
 
-    wfsurl = 'WFS:http://gsn-geose-loadbala-17usyyb36bfdl-1788485819.us-east-1.elb.amazonaws.com/geoserver/piazza/wfs?service=wfs&version=2.0.0'
-    #wfsurl = 'WFS:http://gsp-geose-loadbala-4ep8ufue9sxl-919040015.us-east-1.elb.amazonaws.com/geoserver/piazza/wfs?service=wfs&version=2.0.0'
-    layername = '2d791d6c-dabb-4b28-8d23-d1d78d8d29ff'
+    imgurl = 'http://landsat-pds.s3.amazonaws.com/L8/139/045/LC81390452014295LGN00/LC81390452014295LGN00_B3.TIF'
+
+    wfsurl = os.environ.get('WFS_URL')
+    layer = os.environ.get('LAYER')
+
     bbox = [-78.5964604299, -11.1797039494, -76.5056457676, -9.06874079313]
     bbox_32618 = [107085.0, -1236285.0, 334515.0, -1004385.0]
 
     def setUp(self):
-        Options.set_verbose(4)
-
-    def open_wfs(self):
-        """ Open WFS """
-        return beachfront.open_wfs(self.wfsurl, self.layername)
+        Options.set_verbose(5)
 
     def test_open_wfs(self):
         """ Open WFS and check number of features """
-        ds, layer = self.open_wfs()
-        self.assertEqual(layer.GetFeatureCount(), 9620)
+        wfs, layer = mask.fetch_wfs(self.wfsurl, self.layer)
+        self.assertEqual(layer.GetFeatureCount(), 10686)
 
     def test_get_features(self):
-        """ Get only features within a bounding box """
-        ds, layer = self.open_wfs()
-        layer = beachfront.get_features(layer, self.bbox)
+        """ Open WFS using bounding box """
+        wfs, layer = mask.fetch_wfs(self.wfsurl, self.layer)
+        features = mask.get_features(layer, bbox=self.bbox)
         self.assertEqual(layer.GetFeatureCount(), 4)
 
-    def test_get_unioned_features(self):
+    def test_get_features_unioned(self):
         """ Get union of all features within bounding box """
-        ds, layer = self.open_wfs()
-        poly = beachfront.get_unioned_features(layer, self.bbox)
-        self.assertEqual(poly['coordinates'][0][0][0], -78.0)
+        wfs, layer = mask.fetch_wfs(self.wfsurl, self.layer)
+        poly = mask.get_features(layer, bbox=self.bbox, union=True)
+        self.assertAlmostEqual(poly['coordinates'][0][0][0], -77.5029, 4)
 
-    def _test_coordinate_transform(self):
-        """ Convert coordinates a UTM zone (zone 18N) to EPSG:4326 """
-        bbox = beachfront.transform_coords(self.bbox_32618)
-        self.assertAlmostEqual(self.bbox[0], bbox[0])
-        self.assertAlmostEqual(self.bbox[1], bbox[1])
-        self.assertAlmostEqual(self.bbox[2], bbox[2])
-        self.assertAlmostEqual(self.bbox[3], bbox[3])
+    def _test_rasterize_layer(self):
+        """ Rasterize a layer (GDAL 2 only) """
+        ds, layer = mask.fetch_wfs(self.wfsurl, self.layer, bbox=self.bbox)
+        poly = mask.union_features(layer)        
+        geoimg = mask.rasterize_layer(layer)
 
+    def test_mask_with_wfs(self):
+        """ Mask image with a WFS using gippy cookie_cutter """
+        geoimg = download_image(self.imgurl)
+        geoimg.set_nodata(0)
+        imgout = mask.mask_with_wfs(geoimg, self.wfsurl, self.layer)
+        self.assertEqual(imgout.nbands(), 1)
